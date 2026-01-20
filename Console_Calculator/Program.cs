0dbeeaecd86d5eb446d65af054f094a211b2f4a1
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-01-21 02:36:16 +0800

    каркас

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+class Program
+{
+   static bool ValidateParentheses(string brackets)
+   {
+      
+   }
+   static bool ValidateAllBrackets(string brackets)
+   {
+      
+   }
+}
