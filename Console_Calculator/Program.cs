9b674e8e8b172ceb84722d5d4b13d6350ae9a375
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-01-21 21:43:57 +0800

    Добавлено статическое свойство для хранения пар скобок. А также метод, проверяющий, является ли строка скобочной последовательностью

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -2,12 +2,23 @@
 
 class Program
 {
-   static bool ValidateParentheses(string brackets)
+   public static Dictionary<char, char> Brackets { get; } = new Dictionary<char, char>
    {
-      
-   }
+     {'(', ')'},
+     {'[', ']'},
+     {'{', '}'}
+   };
+
    static bool ValidateAllBrackets(string brackets)
    {
-      
+
+   }
+   static bool IsStringBracketSequence(string str)
+   {
+      foreach (char s in str)
+         if (!(Brackets.ContainsKey(s) || Brackets.ContainsValue(s)))
+            return false;
+
+      return true;
    }
 }
