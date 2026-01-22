c0d0b106b1f8bc95145b14090e78aaaaf32d8858
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-01-22 09:02:14 +0800

    Объединение: каркас Main с валидацией скобок

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public delegate Double MathOperationDelegate(double num1, double num2);
+public delegate double MathOperationDelegate(double num1, double num2);
 
 class Program
 {
+   public static Dictionary<char, char> Brackets { get; } = new Dictionary<char, char>
+   {
+     {'(', ')'},
+     {'[', ']'},
+     {'{', '}'}
+   };
+
    static void Main()
    {
       while (true)
@@ -11,7 +19,43 @@ class Program
          string input = Console.ReadLine() ?? "exit";
 
          if (input == "exit") break;
+
+         if (!IsStringBracketSequence(input)) continue;
+
+         if (!ValidateAllBrackets(input)) continue;
       }
+   }
+
+   static bool ValidateAllBrackets(string brackets)
+   {
+      if (!IsStringBracketSequence(brackets)) return false;
+
+      Stack<char> stack = new Stack<char>();
+
+      foreach (char currentBracket in brackets)
+      {
+         if (Brackets.ContainsKey(currentBracket))
+            stack.Push(currentBracket);
+         else if (Brackets.ContainsValue(currentBracket))
+         {
+            if (stack.Count == 0)
+               return false;
+
+            if (Brackets[stack.Pop()] != currentBracket)
+               return false;
+         }
+      }
+
+      return stack.Count == 0;
+   }
+
+   static bool IsStringBracketSequence(string str)
+   {
+      foreach (char s in str)
+         if (!(Brackets.ContainsKey(s) || Brackets.ContainsValue(s)))
+            return false;
+
+      return true;
    }
 }
 class Operation
