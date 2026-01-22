22e6dc03830a9b7e6c836fd17f66e8605cbe7d60
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-01-22 10:20:41 +0800

    Добавлен метод для парсинга текстовой записи в массив типа object, а так же немного логики в Main

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -17,10 +17,24 @@ class Program
       {
          string input = Console.ReadLine() ?? "exit";
 
+         Console.WriteLine("Ваш ввод с точки зрения скобок: " +
+         (ValidateAllBrackets(input) ? "корректен" : "некорректен"));
+
+         
+
          if (input == "exit") break;
       }
    }
 
+   // static object[] InfixToPostfix(object[] infix)
+   // {
+   //    List<object> postfix = new List<object>();
+   //    Stack<Operation> operations = new Stack<Operation>();
+
+      
+
+   //    return postfix.ToArray();
+   // }
    static bool ValidateAllBrackets(string brackets)
    {
       brackets = StringToBracketSequence(brackets);
@@ -52,6 +66,42 @@ class Program
             result += s;
 
       return result;
+   }
+
+   static object[] ParseInfixExpression(string expression)
+   {
+      List<object> tokens = new List<object>();
+      string numberBuffer = "";
+
+      foreach (char c in expression)
+      {
+         if (char.IsDigit(c) || c == '.')
+         {
+            numberBuffer += c;
+         }
+         else if (char.IsWhiteSpace(c))
+         {
+            if (numberBuffer != "")
+            {
+               tokens.Add(double.Parse(numberBuffer));
+               numberBuffer = "";
+            }
+         }
+         else if (Brackets.ContainsKey(c) || Brackets.ContainsValue(c) || "+-*/^".Contains(c))
+         {
+            if (numberBuffer != "")
+            {
+               tokens.Add(double.Parse(numberBuffer));
+               numberBuffer = "";
+            }
+            tokens.Add(c);
+         }
+      }
+
+      if (numberBuffer != "")
+         tokens.Add(double.Parse(numberBuffer));
+
+      return tokens.ToArray();
    }
 }
 class Operation
