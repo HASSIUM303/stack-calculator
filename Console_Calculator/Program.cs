015c40fbd7b215fe742290d0c027da277d4d8d49
HASSIUM303 <urunbaevhasan814@gmail.com>
2026-01-22 08:11:44 +0800

    Добавлен класс для операций, делегат для поддержки событий класса, поля и конструктор для полей

--- Console_Calculator/Program.cs
+++ Console_Calculator/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+
+public delegate Double MathOperationDelegate(double num1, double num2);
+
+class Program
+{
+   static void Main()
+   {
+
+   }
+}
+class Operation
+{
+   public string SymbolName { get; }
+   public uint Priority;
+   public bool IsDependenceOfOrder { get; }
+   public event MathOperationDelegate OperationMeth
+   {
+      add { OperationMeth += value; }
+      remove { }
+   }
+
+   public Operation(string SymbolName, int Priority, MathOperationDelegate meth, bool isOrder)
+   {
+      this.SymbolName = SymbolName;
+
+      if (Priority >= 0) this.Priority = (uint)Priority;
+      else this.Priority = 0;
+
+      OperationMeth += meth;
+
+      IsDependenceOfOrder = isOrder;
+   }
+}
