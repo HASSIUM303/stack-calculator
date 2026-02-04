3137ee13337e5335cf6ff54688e64cac2bbce3aa
urunbaevhasan <urunbaevhasan814@gmail.com>
2026-02-04 23:25:59 +0800

    Добавлены классы операций

--- Console_Calculator/Operations.cs
+++ Console_Calculator/Operations.cs
@@ -17,6 +17,32 @@ class AddOperation : OperationBase
    public AddOperation(string symbol, int priority) : base(symbol, priority) { }
    public override double Apply(double a, double b) => a + b;
 }
+class SubtractOperation : OperationBase
+{
+   public SubtractOperation(string symbol, int priority) : base(symbol, priority) { }
+   public override double Apply(double a, double b) => a - b;
+}
+class MultiplyOperation : OperationBase
+{
+   public MultiplyOperation(string symbol, int priority) : base(symbol, priority) { }
+   public override double Apply(double a, double b) => a * b;
+}
+
+class DivideOperation : OperationBase
+{
+   public DivideOperation(string symbol, int priority) : base(symbol, priority) { }
+   public override double Apply(double a, double b) => a / b;
+}
+
+class PowerOperation : OperationBase
+{
+   public PowerOperation(string symbol, int priority) : base(symbol, priority) { }
+   public override double Apply(double a, double b) => Math.Pow(a, b);
+}
+
+
+
+
 
 class Operation
 {
