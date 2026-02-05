4b127c2435b5d85dd562887e1597c4a4d2c028c3
urunbaevhasan <urunbaevhasan814@gmail.com>
2026-02-06 02:48:44 +0800

    Удалён устаревший класс Operation

--- Console_Calculator/Operations.cs
+++ Console_Calculator/Operations.cs
@@ -39,31 +39,3 @@ class PowerOperation : OperationBase
    public PowerOperation(string symbol, int priority) : base(symbol, priority) { }
    public override double Apply(double a, double b) => Math.Pow(a, b);
 }
-
-
-
-
-
-class Operation
-{
-   public string SymbolName { get; }
-   public uint Priority;
-   public MathOperationDelegate OperationMeth { get; private set; }
-
-   public Operation(string SymbolName, int Priority, MathOperationDelegate meth)
-   {
-      this.SymbolName = SymbolName;
-
-      if (Priority >= 0) this.Priority = (uint)Priority;
-      else this.Priority = 0;
-
-      OperationMeth = meth;
-   }
-
-   public override string ToString()
-   {
-      return this.SymbolName;
-   }
-}
-
-public delegate double MathOperationDelegate(double num1, double num2);
